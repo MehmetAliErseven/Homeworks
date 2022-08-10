935f86ebe1c0f0bfdbf5d095ed1b4b93f4bbf5ad
Mehmet Ali Erseven <72468337+MehmetAliErseven@users.noreply.github.com>
2022-08-10 12:51:14 +0300

    Add files via upload

---
+++ post-form/post_form.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<form action="post_form.php" method="post">
+    <label for="number">Num</label>
+    <input type="number" name="number" id="number">
+    <button type="submit" name="submit">Submit</button>
+</form>
+<br>
+<?php
+
+$num = $_POST["number"];
+
+function formCount($num) {
+    if($num == null) {
+        echo "Bir değer giriniz!";
+    } elseif ($num % 3 == 0) {
+        $a = $num / 3;
+        echo $num . " sayısı 3 sayısına tam bölünebilir. Sonuç: " . $a . " sayısıdr.";
+    } else {
+        echo $num . " sayısı 3 e tam bölünmez, ";
+        $b = 0;
+        while ($b % 3 == 0) {
+            $b ++;
+        }
+        echo $b . " sayısı 3 sayısına tam bölünebilir.";
+    }
+
+}
+
+formCount($num);
+
+
+?>
+
+
+</body>
+</html>
